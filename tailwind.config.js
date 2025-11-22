089a9cfdfc42e30de5ece41b01df29c25d837408
Hasrat Afridi <148011899+Hasrat270@users.noreply.github.com>
2025-11-22 19:50:51 +0500

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#e45447',
+        secondary: '#ffd200',
+        dark: '#222222',
+        gray: '#777777',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
