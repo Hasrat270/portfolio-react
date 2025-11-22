808a11319fbecb2b9d56dc604c836cd06df97c21
Hasrat Afridi <148011899+Hasrat270@users.noreply.github.com>
2025-11-22 19:51:04 +0500

    Add src/pages/Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Banner from '../components/Banner';
+import Portfolio from '../components/Portfolio';
+import About from '../components/About';
+import Testimonials from '../components/Testimonials';
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+      <Portfolio />
+      <About />
+      <Testimonials />
+    </>
+  );
+};
+
+export default Home;
